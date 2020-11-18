441fb4b0a395a3ce57af1afbd624648d862afcf4
Ghaith Darwish <ghaith.darwish01@stclairconnect.ca>
2020-11-18 14:23:24 -0500

    Create DetailEvent UI,  Implement Detail Page #6

---
+++ NextEvent/NextEvent/Screens/DetailEventViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailEventViewController.swift
+//  NextEvent
+//
+//  Created by gaith darwish on 2020-11-18.
+//
+
+import UIKit
+
+class DetailEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
